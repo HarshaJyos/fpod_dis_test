8b9ca6290c3b47cba39ff5260bfab3763c645334
HarshaJyos <harshajyosyabhatla@gmail.com>
2026-07-24 08:55:48 +0530

    Configure dynamic Firebase config loading via environment variables

--- server.js
+++ server.js
@@ -36,13 +36,13 @@ const { initializeApp } = require('firebase/app');
 const { getFirestore, doc, onSnapshot } = require('firebase/firestore');
 
 const firebaseConfig = {
-  apiKey: "AIzaSyA03ON8h3-Txnn12LsqwinTZMzDbddd1nc",
-  authDomain: "freshpod-901ed.firebaseapp.com",
-  projectId: "freshpod-901ed",
-  storageBucket: "freshpod-901ed.firebasestorage.app",
-  messagingSenderId: "209820777199",
-  appId: "1:209820777199:web:2ac6487aad0df9b521b91a",
-  measurementId: "G-K2G9M145ZH"
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
+  measurementId: process.env.FIREBASE_MEASUREMENT_ID
 };
 
 const firebaseApp = initializeApp(firebaseConfig);
@@ -78,6 +78,19 @@ app.get('/login', (req, res) => {
 
 app.get('/dashboard', (req, res) => {
   res.sendFile(__dirname + '/public/dashboard.html');
+});
+
+// Serve Firebase configuration dynamically to front-end to keep credentials out of code files
+app.get('/api/firebase-config', (req, res) => {
+  res.json({
+    apiKey: process.env.FIREBASE_API_KEY,
+    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.FIREBASE_APP_ID,
+    measurementId: process.env.FIREBASE_MEASUREMENT_ID
+  });
 });
 
 // API endpoint for dashboard to fetch payments list
